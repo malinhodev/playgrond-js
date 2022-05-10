19203d36629229d56a59431ef6f5dec0212abb40
Marlon Ferreira <100293149+malinhodev@users.noreply.github.com>
2022-05-10 12:41:10 -0300

    variaveis-inicio

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ console.log(!("marlon" == "marlon"));
 /**exe. 05 
 console.log('marlon' * 2);
 */
+
+
+let nome = "marlon", sobrenome = "ferreira";
+
+console.log(nome);
+console.log(`meu nome é ${nome}`);
+console.log(`meu nome é `+ nome);
+
+
+nome = 5
+
+console.log(nome * nome);
+
+console.log(nome,sobrenome);
+
+console.log("meu sobrenome é "+ sobrenome);
+
