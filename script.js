981b6e5d2a2d37146c0697d1aeaf9bb12c663e32
Marlon Ferreira <100293149+malinhodev@users.noreply.github.com>
2022-05-12 18:29:36 -0300

    .

--- script.js
+++ script.js
@@ -41,5 +41,69 @@ let arredondarPraCima = Math.ceil(2.387825);
 
 console.log(arredondarPraCima);
     */
+/*** 
+const nome = 'marlon';
+
+if(nome == 'marlon') {
+    console.log(" ola marlon!");
+} else {
+    console.log(" Não te conheço!");
+}
+*/
 
 
+//console.log(Math.pow(2,5));
+/****
+let condutor = 20;
+let cnh = false;
+
+if(condutor >= 18 && cnh == true ) {
+    console.log('pode dirigir!');
+    
+} else if(condutor >= 18 && cnh == false){
+    console.log('não pode dirigir, porém hapito para emitir CNH!');
+} else{
+    console.log('não pode dirigir!');
+}
+ */
+/************WHILE********* 
+let i = 0;
+
+while(i <= 10) {
+    console.log(i);
+    i = i + 1;
+  }
+  */
+ /************FOR******** 
+
+ for (let i = 100; i >= 50; i-- ) {
+     console.log(i);
+
+ }
+
+ */
+/*********imprimindo par e ímpar no looping*****
+for(i = 0; i <= 50; i++){
+    if(i % 2 == 0){
+        console.log(`o numero ${i} é par`);
+    }else{
+        console.log(`o numero ${i} é ímpar`);
+    }
+}
+ */
+
+/*********numero primo**** */
+let num = 41;
+let divisoes = 0;
+
+for (i = 1; i <= Number; i++){
+    if(num % i == 0){
+        divisoes++;
+    }
+}
+
+if(divisoes == 2) {
+    console.log(`o número ${num} é primo`);
+} else {
+    console.log(`o número ${num} não é primo`);
+}
