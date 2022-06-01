713de9466554c8acc9ac43cd2e30b0a27106a5a0
Marlon Ferreira <100293149+malinhodev@users.noreply.github.com>
2022-06-01 10:41:13 -0300

    aprendendo basa o-ob

--- orientação-o-b/script.js
+++ orientação-o-b/script.js
@@ -107,7 +107,7 @@ pastor.setCor = 'marrom e preto';
 
 console.log(pastor.getCor);*/
 
-/************** HERANÇA-(INHERITANCE IN ENGLISH)************ */
+/************** HERANÇA-(INHERITANCE IN ENGLISH)************ 
 //Uma classe pode herdar propriedades de outra classe por herança
 //para isso utilizamos: extends
 //para setar as propriedades da classe pai utilizamos: super
@@ -133,11 +133,60 @@ class Cachorro extends Mamifero {
 
 let pug = new Cachorro(4,"pug");
 console.log(pug.patas);
-pug.latir();
+pug.latir();*/
 
-/************* instaceof ************* */
+/************* instaceof ************* 
 //Podemos verificar quem é o pai do obj. utilizando: instaceof
 //resumindo é o typof dos objetos
 
 console.log(new Cachorro instanceof Mamifero);//true pois cachorro é uma instacia de mamifero
+*/
 
+/*======== Propriedades do Objeto=========*/
+//As propriedades são as caracteriticas dos objetos
+// EX: cor, portas, nome, marca e etc...
+//Podemos iniciar um objeto com várias propriedades
+//E acessa-las para resgatar os seus valores
+
+let carro = {
+    portas: 4,
+    cor: 'verde',
+    opicionais: ["teto solar", "blindagem", "ar condicionado"],
+    revisado: true,
+    "tem modo eco": false,//declaração com mais de uma palavra.
+}
+
+console.log(carro.portas);
+console.log(carro['portas']);
+
+//podemos criar condições
+
+if(carro.portas > 2){
+    console.log("este carro tem mais de duas portas!!!")
+}
+
+
+       //Tipos de de dados e propriedades
+//as propriedades aceitam qualquer tipo de dados do javascript
+//booleanos, numbers, strings e arrays
+
+console.log(carro.revisado);
+console.log(carro.opicionais);
+
+for(let i = 0; i < carro.opicionais.length; i++) {
+    console.log(carro.opicionais[i]);
+}
+
+if(carro.revisado == true){
+    console.log("fazer revisão após 10.000km rodados.");
+}
+
+    //Propriedades com mais de uma palavra
+console.log(carro["tem modo eco"]);
+
+//Acessando propriedades por variáveis
+let a = 'cor';//cor é uma proriedade do obj. carro.
+console.log(carro[a]);//ira imprimir o valor da propriedade cor.
+
+ //carro.cor = "vermelho";
+ //console.log(carro[a]);
