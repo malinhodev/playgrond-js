274a5311401f82bcaa6a3552fc8f726938bff079
malinhodev <mcterasgo@gmail.com>
2022-07-11 10:14:20 -0300

    finalizando eventos DOM

--- evebtos DOM/eventdom.js
+++ evebtos DOM/eventdom.js
@@ -33,3 +33,33 @@ btn3.addEventListener("click", (e)=>{
 //stopPropagation(); da stop na propagação, se uma tag
 //estiver dentro de outra com evento.
 
+/*****Ações default
+ * preventDefaul(); para parar os eventos pré definidos
+ * ======================================================
+ * ***Evento por tecla
+ * keyup - quando soltamos a tecla
+ * keydow - quando apertamos a tecla
+ * o mais utilizado é o keyup
+ */
+window.addEventListener("keyup", function(e){
+    if(e.key == "Enter"){
+        console.log("soltou o enter");
+    }
+});
+/** evento do mouse "mousemove"
+**evento do scroll do mouse "scroll"
+*/
+window.addEventListener("scroll", function(e){
+    if(window.pageYOffset > 1000){
+        console.log("ativou");
+    }
+});
+/** eventofoco
+ * quando da foco no input
+ * "blur" - quando sai do input
+ * "foco" - quando entra no input
+ * 
+ * *page load
+ * "load" - quando ta carreegando a pagina
+ * "beforeunload" - quando vai fechar a página
+ */
